1080169f6561a2175b527c21c78e1e339b71fc53
Andrej <138933791+Andrej-Clarey@users.noreply.github.com>
2023-07-24 17:23:53 -0400

    xfer message and command

--- spec/command.spec.js
+++ spec/command.spec.js
@@ -6,7 +6,24 @@ const Command = require('../command.js');
 describe("Command class", function() {
 
   it("throws error if command type is NOT passed into constructor as the first parameter", function() {
-    expect( function() { new Command();}).toThrow(new Error('Command type required.'));
+    let testFunction = function() { 
+      new Command(); 
+    };
+    expect(testFunction).toThrow(new Error('Command type required.'));
+  });
+
+  it("constructor sets command type", function() {
+    // create a new Command object
+    let obj = new Command('MOVE', 6);
+    // write an expectation to verify that the commandType property was set
+    expect(obj.commandType).toEqual('MOVE');
+  });
+
+  it("constructor sets a value passed in as the 2nd argument", function() {
+    // create a new Command object
+    let obj = new Command('MOVE', 6);
+    // write an expectation to verify that the value property was set
+    expect(obj.value).toEqual(6);
   });
 
 });
