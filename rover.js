0b8cb6b666e95c5cef3cdc4f3ac1bb37c6469fc7
Andrej <138933791+Andrej-Clarey@users.noreply.github.com>
2023-07-25 11:54:14 -0400

    start test 8

--- rover.js
+++ rover.js
@@ -1,5 +1,65 @@
+const Message = require('./message.js');
+const Command = require('./command.js');
+
+// let commands = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
+// let message = new Message('Test message with two commands', commands);
+// let rover = new Rover(98382);    // Passes 98382 as the rover's position.
+// let response = rover.receiveMessage(message);
+
+
 class Rover {
-   // Write code here!
+   
+  // create constructor that gets tests to pass (Test 7)
+  constructor(position){
+    this.position = position;
+    this.mode = 'NORMAL';
+    this.generatorWatts = 110;
+  }
+    // create receiveMessage(message) method
+  recieveMessage(message){
+    // let newCommands = message.commands
+    for(commands in message){
+      
+    }
+  };
+
+
+    // create response object, and set its message property (Test 8)
+
+    // add a results property to response object, as an empty array (Test 9)
+
+    // for each command in the message, add an item to the results array (Test 9)
+
+      // for each command, add {} (an empty object) (Test 9)
+
+      // if the command.commandType is STATUS_CHECK (Test 10) 
+
+        // create a result object (empty to start)
+
+        // set result.completed = true;
+
+        // add a roverStatus object to the result object, with the status values of the rover
+
+        // add the result object to the results array
+
+      // if command.commandType is MODE_CHANGE (Test 11)
+
+        // create a result obj
+
+        // set result.completed = true
+
+        // update rover's mode
+
+        // add result to the results array
+
+    // if command.commandType is MOVE (Test 12)
+
+      // if rover mode is LOW_POWER, don't move and send competed = false in response
+
+      // else if mode is NORMAL, change the position and send completed = false in response (Test 13)
+  
+    // return the response object
+  
 }
 
 module.exports = Rover;
--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -8,6 +8,93 @@ const Command = require('../command.js');
 
 describe("Rover class", function() {
 
-  // 7 tests here!
+  // Test 7: "constructor sets position and default values for mode and generatorWatts"
+  it("constructor sets position and default values for mode and generatorWatts", function(){
+    // create a new Rover object w/ a given initial position
+    let rover = new Rover(42);
+    
+    // check that rover's position is correct
+    expect(rover.position).toEqual(42);
+    // check that mode is correct
+    expect(rover.mode).toEqual('NORMAL');
+    // check that generatorWatts is correct
+    expect(rover.generatorWatts).toEqual(110);
+  });
+
+  // Test 8
+  it("response returned by receiveMessage contains name of message", function(){
+    let message = new Message("MSG005", []);
+    let rover = new Rover(42);
+    let response = rover.receiveMessage(message);
+
+    // verify that response.message has the correct value
+  });
+
+  // Test 9
+  it("response returned by receiveMessage includes two results if two commands are sent in the message", function(){
+
+    // build test objects: 1) Message w/ 2 commands and 2) a rover
+
+    // pass the message to the rover by calling rover.receiveMessage
+
+    // verify that response.results.length is 2
+    
+  });
+
+  // TEST 10
+  it("responds correctly to status check command", function(){
+
+    // build test objects: 1) Message w/ STATUS_CHECK command and 2) a rover
+
+    // pass the message to the rover by calling rover.receiveMessage
+
+    // verify response.results:
+    //   - get the single item (the result) from the array
+    //   - verify that the result has a completed property that is true
+    //   - verify that the result has a roverStatus property w/ correct values (3 tests)
+
+      
+  });
+
+  // Test 11
+  it("responds correctly to mode change command", function(){
+
+    // build test objects: 1) Message w/ MODE_CHANGE command and 2) a rover
+
+    // pass the message to the rover by calling rover.receiveMessage
+
+    // verify that response.results contains the correct object 
+
+    // verify that the rover's mode property was updated 
+    
+  });
+
+  // Test 12
+  it("responds with false completed value when attempting to move in LOW_POWER mode", function(){
+
+    // build rover object
+
+    // send a message+command to change into LOW_POWER mode
+
+    // send a message+command to attempt to MOVE the rover
+
+    // verify that the given result object has completed === false
+
+    // verify that the rover's position did not change
+    
+  });
+
+  // Test 13
+  it("responds with position for move command", function(){
+
+    // build a rover, and also build a message with a MOVE command
+
+    // send the message to the rover
+
+    // verify that the result object has completed === false
+
+    // verify that the rover has the correct position value 
+    
+  });
 
 });
